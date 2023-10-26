58a9a312c943b40e59b4bab6ed647e9d33aa2222
Paul Sites <pts211@gmail.com>
2023-10-26 15:59:54 -0500

    WIP

--- src/core/task/task.service.ts
+++ src/core/task/task.service.ts
@@ -1,25 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { JobbossConnectService } from 'src/modules/jobboss-connect/jobboss-connect.service';
-import { MatrixConnectService } from 'src/modules/matrix-connect/matrix-connect.service';
 // import { MqttService } from '../../modules/bpm/bpm.service';
 
 @Injectable()
 export class TaskService {
-  constructor(
-    private readonly jbService: JobbossConnectService,
-    private readonly maService: MatrixConnectService,
-  ) {}
+  constructor() {}
 
   @Cron(CronExpression.EVERY_10_SECONDS)
   async everyTenSec() {
     // console.log('everyTenSec');
     // if (!this.mqttService.isActive()) return;
     // this.mqttService.publish(MqttTopic.ChangeEvents, JSON.stringify('res'));
-  }
-
-  @Cron(CronExpression.EVERY_2_HOURS)
-  async everyTwoHours() {
-    this.maService.resetCostCenterTransactionDescriptions();
   }
 }
--- src/modules/matrix-connect/dto/update-matrix-connect.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMatrixConnectDto } from './create-matrix-connect.dto';
-
-export class UpdateMatrixConnectDto extends PartialType(CreateMatrixConnectDto) {}
