cd7a88b88786cbc2516a363bc6683a8e551bbba2
Paul Sites <pts211@gmail.com>
2023-10-29 11:50:29 -0500

    WIP

---
+++ src/modules/shared/Cookie.ts
@@ -0,0 +1,37 @@
+export class Cookie {
+  sessionId: string;
+  sessionKey: string;
+  accessRights: string;
+  accessLevel: string;
+
+  constructor(cookieHeader?: string[]) {
+    this.sessionId = '';
+    this.sessionKey = '';
+    this.accessRights = '';
+    this.accessLevel = '';
+
+    if (cookieHeader) {
+      this.extractValuesFromSetCookie(cookieHeader);
+    }
+  }
+
+  extractValuesFromSetCookie(setCookieHeader) {
+    if (setCookieHeader) {
+      this.sessionId = this.extractCookieValue(setCookieHeader, 'Session-Id');
+      this.sessionKey = this.extractCookieValue(setCookieHeader, 'Session-Key');
+      this.accessRights = this.extractCookieValue(
+        setCookieHeader,
+        'AccessRights',
+      );
+      this.accessLevel = this.extractCookieValue(
+        setCookieHeader,
+        'AccessLevel',
+      );
+    }
+  }
+
+  extractCookieValue(cookies, name) {
+    const cookie = cookies.find((cookie) => cookie.startsWith(`${name}=`));
+    return cookie ? cookie.split(';')[0].split('=')[1] : '';
+  }
+}
