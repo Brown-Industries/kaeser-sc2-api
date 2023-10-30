8e609d356875a1066cedf03a60478f48444e19dc
Paul Sites <pts211@gmail.com>
2023-10-30 10:52:47 -0500

    WIP

---
+++ src/modules/maintenance/dto/messages.dto.ts
@@ -0,0 +1,36 @@
+export class Message {
+  messageType: string;
+  messageDate: Date;
+  message: string;
+
+  constructor(data: any) {
+    this.messageType = data.ReportTypeTxt.replace('message', '').trim();
+    const tempDate = data.ReportDateTime.replace('AM', '')
+      .replace('PM', '')
+      .trim();
+    this.messageDate = new Date(tempDate);
+    this.message = data.Text.replace('\n', '').trim();
+  }
+}
+
+export class MessagesDto {
+  messages: Message[];
+
+  constructor(data: any) {
+    this.messages = [];
+
+    if (data && data['3'] && typeof data['3'] === 'object') {
+      for (const key in data['3']) {
+        if (data['3'].hasOwnProperty(key)) {
+          const messageData = data['3'][key];
+          const message = new Message(messageData);
+          this.messages.push(message);
+        }
+      }
+    }
+  }
+
+  toObj() {
+    return this.messages;
+  }
+}
