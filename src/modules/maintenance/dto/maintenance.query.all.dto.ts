c3fcf2e44e56d58685c056bb4ee777e41c5f7627
Paul Sites <pts211@gmail.com>
2024-02-12 11:55:54 -0600

    0.3.0: Project cleanup, documentation

--- src/modules/maintenance/dto/maintenance.query.all.dto.ts
+++ src/modules/maintenance/dto/maintenance.query.all.dto.ts
@@ -1,21 +1,9 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional } from 'class-validator';
 import { IsEnumKey } from 'src/modules/shared/CustomValidators';
-import {
-  CompressorData,
-  MaintenceTimerData,
-} from 'src/modules/shared/enum/CompressorData';
+import { CompressorData } from 'src/modules/shared/enum/CompressorData';
 
 export class MaintenanceQueryAllDTO {
-  // @ApiPropertyOptional({
-  //   enum: Object.keys(MaintenceTimerData).filter(
-  //     (key) => typeof key === 'string',
-  //   ),
-  // })
-  // @IsOptional()
-  // @IsEnumKey(MaintenceTimerData)
-  // maintenceTimer: MaintenceTimerData[];
-
   @ApiPropertyOptional({
     enum: Object.keys(CompressorData).filter((key) => typeof key === 'string'),
   })
